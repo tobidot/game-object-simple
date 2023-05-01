f72727af149e1d424cc3f82ec1245ca1f2401497
tobidot <object.name@live.de>
2023-05-01 20:37:40 +0200

    update upload script

--- bin/update.sh
+++ bin/update.sh
@@ -5,6 +5,6 @@ git pull
 # run the migrations
 /opt/RZphp82/bin/php-cli artisan migrate --force
 # install npm dependencies
-npm install --only=prod
+npm ci --only=prod
 # build the assets
 npm run build
