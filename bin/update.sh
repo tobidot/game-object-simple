e021c595517100f3a4576942901be5ab223e1132
tobidot <object.name@live.de>
2023-05-01 20:27:45 +0200

    style the trix images

--- bin/update.sh
+++ bin/update.sh
@@ -1,10 +1,10 @@
 # Update the project
 git pull
 # install composer dependencies
-/opt/RZphp82/bin/php-cli /mnt/web506/e1/17/511267217/htdocs/composer.phar --working-dir="$(pwd)" install
+/opt/RZphp82/bin/php-cli /mnt/web506/e1/17/511267217/htdocs/composer.phar --working-dir="$(pwd)" install --no-dev --no-interaction --prefer-dist --optimize-autoloader
 # run the migrations
 /opt/RZphp82/bin/php-cli artisan migrate --force
 # install npm dependencies
-npm install
+npm install --only=prod
 # build the assets
 npm run build
